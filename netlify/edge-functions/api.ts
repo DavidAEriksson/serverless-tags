415f0ca798fd17ab9959c6d508e8c052c3edfa1a
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:54:13 +0100

    fix: text

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -4,9 +4,8 @@ export default async (request: Request, context: Context) => {
   console.log("################# METHOD", request.method);
   console.log("################# HEADERS", request.headers);
   if (request.method === "POST") {
-    /* const json_data = await request.json(); */
-    /* console.log("################# JSON_DATA: ", json_data); */
-    console.log("################# stringified", JSON.stringify(request.text));
+    const data = await request.text();
+    console.log("################# DATA: ", data);
   }
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

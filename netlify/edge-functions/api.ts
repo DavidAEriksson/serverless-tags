daf164a5d129a669b5779478347badb74379deb7
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:13:20 +0100

    fix: better logs

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -1,10 +1,10 @@
 import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
-  console.log(request);
+  console.log("################# REQUEST:", request);
   const body = request.body;
   console.log("################# BODY: ", body);
-  console.log(context);
+  console.log("################# CONTEXT: ", context);
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },
   });

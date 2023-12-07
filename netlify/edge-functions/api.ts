14f3a051c22a8ff3b2d5c012eca0d224a6ec8f6c
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:07:13 +0100

    fix: function json

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,7 +2,7 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log(request);
-  console.log("################# REQUEST JSON: ", request.json);
+  console.log("################# REQUEST JSON: ", request.json());
   console.log(context);
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

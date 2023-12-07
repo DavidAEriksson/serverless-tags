d96fd9b5846ce35fba6c09864d4912a43c6eff1c
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 13:44:19 +0100

    feat: post request

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -5,6 +5,7 @@ const api = express();
 
 const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
+router.post("/hello", (req, res) => console.log(req.body));
 
 api.use("/api/", router);
 

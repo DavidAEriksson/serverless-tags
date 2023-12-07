d81f91dcbc9e107a629a01130046df2f613db586
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 13:49:12 +0100

    fix: run server

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+const port = process.env.PORT || 3000;
 const api = express();
 
 const router = Router();
@@ -10,3 +11,7 @@ router.post("/hello", (req, res) => console.log(req.body));
 api.use("/api/", router);
 
 export const handler = serverless(api);
+
+api.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});

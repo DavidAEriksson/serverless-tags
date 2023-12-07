7fa6595fd2e30b4b533d449b0fa0cacb78a52027
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:10:02 +0100

    fix: get body from body

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,7 +2,7 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log(request);
-  const body = await request.json();
+  const body = request.body;
   console.log("################# BODY: ", body);
   console.log(context);
   return new Response("Hello, World!", {

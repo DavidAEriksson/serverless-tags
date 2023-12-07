2630757c996d2042fc8547910764380d7f31aa38
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:05:47 +0100

    fix: add log for json form request

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,9 +2,10 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log(request);
+  console.log("################# REQUEST JSON: ", request.json);
   console.log(context);
   return new Response("Hello, World!", {
-    headers: { "content-type": "text/json" }
+    headers: { "content-type": "text/json" },
   });
 };
 

1f1f0544bcf26e73760bba2a223156a10940cef9
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 14:42:51 +0100

    fix: dont use lambda

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,15 +1,5 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
+import type { Context } from "@netlify/functions";
 
-const handler: Handler = async (
-  event: HandlerEvent,
-  context: HandlerContext,
-) => {
-  console.log(event);
-  console.log(context);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Hello World" }),
-  };
+export default async (req: Request, context: Context) => {
+  return new Response("Hello, world!");
 };
-
-export { handler };

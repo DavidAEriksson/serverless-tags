e5049da25c643b63224f065fc491a287fef11ead
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:20:40 +0100

    fix: log json if post

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -5,6 +5,10 @@ export default async (request: Request, context: Context) => {
   console.log("################# REQUEST:", request);
   const body = request.body;
   console.log("################# BODY: ", body);
+  if (request.method === "POST") {
+    const json_data = await request.json();
+    console.log("################# JSON_DATA: ", json_data);
+  }
   console.log("################# CONTEXT: ", context);
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

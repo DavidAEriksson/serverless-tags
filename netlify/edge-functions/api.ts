e0f51d6d990ac232ec74e19465db7df16dddc65b
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:52:03 +0100

    fix: dont parse anything

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -4,8 +4,9 @@ export default async (request: Request, context: Context) => {
   console.log("################# METHOD", request.method);
   console.log("################# HEADERS", request.headers);
   if (request.method === "POST") {
-    const json_data = await request.json();
-    console.log("################# JSON_DATA: ", json_data);
+    /* const json_data = await request.json(); */
+    /* console.log("################# JSON_DATA: ", json_data); */
+    console.log("################# stringified", JSON.stringify(request.text));
   }
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

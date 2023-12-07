e7cbc2f72f88d86b97801126e10573259fc22132
David Eriksson <davideriksson@daer3303chr.local>
2023-12-07 15:08:24 +0100

    fix: await json body

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,7 +2,8 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log(request);
-  console.log("################# REQUEST JSON: ", request.json());
+  const body = await request.json();
+  console.log("################# BODY: ", body);
   console.log(context);
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

2b3d57a231f26fdb6ceec19df10af0219ec71c85
David Eriksson <davideriksson@daer3303chr.lan>
2023-12-08 10:55:20 +0100

    fix: try parse json

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -4,7 +4,7 @@ export default async (request: Request, context: Context) => {
   console.log("################# METHOD", request.method);
   console.log("################# HEADERS", request.headers);
   if (request.method === "POST") {
-    const data = await request.text();
+    const data = await request.json();
     console.log("################# DATA: ", data);
   }
   return new Response("Hello, World!", {
@@ -15,3 +15,20 @@ export default async (request: Request, context: Context) => {
 export const config: Config = {
   path: "/",
 };
+
+/*
+
+TODO: we need to add this:
+
+{
+  "assetId": "{{assetId}}",
+  "categoryId": "{{categoryId}}",
+  "timestamp":{{start}},
+  "eventName":"{{eventName}}",
+  "value":" {{value}}",
+  "confidence": {{confidence}}
+}
+
+as the body of the request in williot so that we can parse it here
+
+*/

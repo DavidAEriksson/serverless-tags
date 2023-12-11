982e0ccededc2c2b1ac89a46e440084f5ea368c4
David Eriksson <davideriksson@daer3303chr.local>
2023-12-11 10:58:46 +0100

    fix: only log active events

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -2,10 +2,11 @@ import type { Config, Context } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
   console.log("################# METHOD", request.method);
-  console.log("################# HEADERS", request.headers);
   if (request.method === "POST") {
-    const data = await request.json();
-    console.log("################# DATA: ", data);
+    const data: WilliotSenseEvent = await request.json();
+    if (data.eventName == "active") {
+      console.log("################# ACTIVE EVENT", data);
+    }
   }
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },
@@ -15,20 +16,3 @@ export default async (request: Request, context: Context) => {
 export const config: Config = {
   path: "/",
 };
-
-/*
-
-TODO: we need to add this:
-
-{
-  "assetId": "{{assetId}}",
-  "categoryId": "{{categoryId}}",
-  "timestamp":{{start}},
-  "eventName":"{{eventName}}",
-  "value":" {{value}}",
-  "confidence": {{confidence}}
-}
-
-as the body of the request in williot so that we can parse it here
-
-*/
---
+++ netlify/utils/types.ts
@@ -0,0 +1,8 @@
+type WilliotSenseEvent = {
+  assetId: string;
+  categoryId: string;
+  timestamp: number;
+  eventName: string;
+  value: string;
+  confidence: number;
+};

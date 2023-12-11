fc54984385adb3e18256dbf7a0f17c5bfc2d6897
David Eriksson <davideriksson@daer3303chr.local>
2023-12-11 11:27:03 +0100

    fix: log all events

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -4,9 +4,7 @@ export default async (request: Request, context: Context) => {
   console.log("################# METHOD", request.method);
   if (request.method === "POST") {
     const data: WilliotSenseEvent = await request.json();
-    if (data.eventName == "active") {
-      console.log("################# ACTIVE EVENT", data);
-    }
+    console.log("################# ACTIVE EVENT", data);
   }
   return new Response("Hello, World!", {
     headers: { "content-type": "text/json" },

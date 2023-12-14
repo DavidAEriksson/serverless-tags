19a56177dd4f975a225d449a75277adbc9ecdc03
David Eriksson <davideriksson@daer3303chr.lan>
2023-12-14 12:03:01 +0100

    feat: push events to chain

--- netlify/edge-functions/api.ts
+++ netlify/edge-functions/api.ts
@@ -1,12 +1,44 @@
 import type { Config, Context } from "@netlify/edge-functions";
+import { createClient, QueryObject } from "postchain-client";
+
+type GetAllTagLogsReturnType = {
+  timestamp: number;
+  asset_name: string;
+  category: string;
+  tag_id: string;
+};
+
+export function getAllTagLogsQueryObject(): QueryObject<
+  GetAllTagLogsReturnType[]
+> {
+  return { name: "tag.get_all_tag_logs", args: undefined };
+}
 
 export default async (request: Request, context: Context) => {
-  console.log("################# METHOD", request.method);
+  const client = await createClient({
+    nodeUrlPool: "https://node0.devnet1.chromia.dev:7740",
+    blockchainRid:
+      "081D30836EACF3B355EF405A2C772BBE13F348EDF0E4E7F4DA0E2864B1EE5FDB",
+  });
   if (request.method === "POST") {
     const data: WilliotSenseEvent = await request.json();
-    console.log("################# ACTIVE EVENT", data);
+    if (data.eventName === "active" && data.value.trim() === "1") {
+      const tx = {
+        operations: [
+          {
+            name: "tag.create_tag_presence_log",
+            args: [data.assetId, data.categoryId, ""],
+          },
+        ],
+        signers: [],
+      };
+      client.addNop(tx);
+      const txRes = await client.sendTransaction(tx);
+      console.log(txRes);
+    }
   }
-  return new Response("Hello, World!", {
+  const bcRes = await client.query(getAllTagLogsQueryObject());
+  return new Response(JSON.stringify(bcRes), {
     headers: { "content-type": "text/json" },
   });
 };
